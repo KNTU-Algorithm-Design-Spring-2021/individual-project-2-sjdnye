bcfd58c68b860b6124c254c4f6c7f8aa314ff8f4
sjdnye <sjdnye@gmail.com>
2021-04-08 00:14:18 +0430

    first test + calculate height

--- colored cubes/Main.py
+++ colored cubes/Main.py
@@ -3,27 +3,51 @@ weight = list()
 dimension = list()
 colors = list()
 temporary = list()
-final_result = 0
-
+LDS = [1 for num in range(0, cubes)]
+result = 0
 for i in range(0, cubes):
-    tmp = int(input(f"enter {i+1}th cubes's weight: "))
+    tmp = int(input(f"enter {i + 1}th cubes's weight : "))
     weight.append(tmp)
-    x = int(input(f"{i+1}th length: "))
-    y = int(input(f"{i+1}th width: "))
-    z = int(input(f"{i+1}th height: "))
+    x = int(input(f"{i + 1}th length : "))
+    y = int(input(f"{i + 1}th width : "))
+    z = int(input(f"{i + 1}th height : "))
     dimension.append((x, y, z))
 
     for j in range(0, 6):
-        R = int(input("enter R: "))
-        G = int(input("enter G: "))
-        B = int(input("enter B: "))
+        R = int(input("enter R : "))
+        G = int(input("enter G : "))
+        B = int(input("enter B : "))
         tup = (R, G, B)
         temporary.append(tup)
 
     colors.append(temporary)
 
+for i in range(0, cubes):
+    for j in range(0, i):
+        if weight[j] > weight[i] and LDS[i] < LDS[j] + 1:
+            LDS[i] = LDS[j] + 1
+            for k in range(0, 6):
+                R2 = 255 - colors[i][k][0]
+                G2 = 255 - colors[i][k][0]
+                B2 = 255 - colors[i][k][0]
+                negative = (R2, G2, B2)
 
+                if negative in colors[j]:
+                    temp = colors[j].index(negative)
+                    if k < 2:
+                        result += dimension[i][0]
+                    elif k > 3:
+                        result += dimension[i][2]
+                    else:
+                        result += dimension[i][1]
 
+                    if temp < 2:
+                        result += dimension[j][0]
+                    elif temp > 3:
+                        result += dimension[j][2]
+                    else:
+                        result += dimension[j][1]
 
+                    break
 
-
+            print(f"the height of tower is : {result}")
